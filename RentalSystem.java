af45921d627da2e17e80af9027eb48460e2bd772
Nawwaf Said <nawwafsaid@trentu.ca>
2025-04-04 23:36:55 -0400

    task 1.1
    Task 1.1: Singleton Pattern
    
    Making the constructor private
    
    Adding a private static instance variable
    
    Adding a public static getInstance() method

--- RentalSystem.java
+++ RentalSystem.java
@@ -3,9 +3,22 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class RentalSystem {
+    private static RentalSystem instance = null; 
+    
     private List<Vehicle> vehicles = new ArrayList<>();
     private List<Customer> customers = new ArrayList<>();
     private RentalHistory rentalHistory = new RentalHistory();
+    
+    private RentalSystem() {
+    
+    }
+    
+    public static RentalSystem getInstance() {
+        if (instance == null) {
+            instance = new RentalSystem();
+        }
+        return instance;
+    }
 
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
